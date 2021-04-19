c617aa3b0ce73c1bb1f3daae68d91b12b9b7d538
Emanuele Beffa <e.beffa@ermes.company>
2021-04-19 23:32:20 +0200

    First commit

---
+++ TestCocoa/TestCocoa.h
@@ -0,0 +1,18 @@
+//
+//  TestCocoa.h
+//  TestCocoa
+//
+//  Created by Emanuele Beffa on 19/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCocoa.
+FOUNDATION_EXPORT double TestCocoaVersionNumber;
+
+//! Project version string for TestCocoa.
+FOUNDATION_EXPORT const unsigned char TestCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoa/PublicHeader.h>
+
+
